038db203f7a71177b87a7eed6d94dcb2a5d323d4
rajdeepthaosen7 <rajdeepthaosen7@gmail.com>
2026-04-25 23:50:26 +0530

    Initial Spring Boot scaffold

---
+++ src/main/java/com/verikyc/repoverikycbackend/RepoVerikycBackendApplication.java
@@ -0,0 +1,13 @@
+package com.verikyc.repoverikycbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RepoVerikycBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RepoVerikycBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/verikyc/repoverikycbackend/RepoVerikycBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.verikyc.repoverikycbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepoVerikycBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
